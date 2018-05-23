bbd24f30a71ba1986f59abfc2204071c9ead11c6
Christine Schulte <christinerschulte@gmail.com>
2018-05-23 15:05:53 -0400

    Store notes in Main's state

--- src/Main.js
+++ src/Main.js
@@ -1,20 +1,40 @@
-import React from 'react'
+import React, {Component} from 'react'
 
 import Sidebar from './Sidebar'
 import NoteList from './NoteList'
 import NoteForm from './NoteForm'
 
-const Main = () =>{
+class Main extends Component{
+    constructor(){
+        super()
+        this.state = {
+            notes: [
+                {
+                  id: 1,
+                  title: 'JS thoughts',
+                  body: 'I just <3 JS so much',
+                },
+                {
+                  id: 2,
+                  title: 'Breakfast',
+                  body: 'Never wake up for it',
+                },
+              ],
+        }
+    }
+    
+    render(){
     return(
         <div 
             className = "Main"
             style={style}
         >
             <Sidebar />
-            <NoteList />
+            <NoteList notes={this.state.notes} />
             <NoteForm />
         </div>
     )
+    }
 }
 
 const style = {
--- src/NoteList.js
+++ src/NoteList.js
@@ -3,36 +3,20 @@ import React from 'react'
 import './NoteList.css'
 import Note from './Note'
 
-const NoteList = () =>{
-    const notes = [
-      {
-        id: 1,
-        title: 'JS thoughts',
-        body: 'I just <3 JS so much',
-      },
-      {
-        id: 2,
-        title: 'Breakfast',
-        body: 'Never wake up for it',
-      },
-      {
-        id: 3,
-        title: 'Class Notes',
-        body: 'REact is swell',
-      },
-    ]
+const NoteList = (props) =>{
+  const notes = props.notes
 
-    return (
-        <div className="NoteList">
-          <h3>Notes</h3>
-          <ul id="notes">
-            <a className="active">
-            { notes.map(note=> <Note key = {note.id} note={note}/>) }
-            </a>
-          </ul>
-        </div>
-      
-    )
+  return (
+      <div className="NoteList">
+        <h3>Notes</h3>
+        <ul id="notes">
+          <a className="active">
+          { notes.map(note => <Note key = {note.id} note={note} />) }
+          </a>
+        </ul>
+      </div>
+    
+  )
 }
 
 export default NoteList
