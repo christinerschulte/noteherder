e1b62db40a02a9325453a380ced6723079108ebd
Christine Schulte <christinerschulte@gmail.com>
2018-05-22 21:11:17 -0400

    Edit Sidebar style from JS to CSS

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -7,100 +7,21 @@ import newHover from './new-hover.png'
 
 const Sidebar = ()=>{
     return(
-        <nav 
-            className="Sidebar"
-            style = {styles.sidebar}    
-        >
-          <div 
-            className="logo"
-            style = {styles.logo}    
-        >
-            <img 
-                src={quill} alt="Noteherder"
-                style={styles.logoImg}    
-            />
+        <nav className="Sidebar">
+          <div className="logo">
+            <img src={quill} alt="Noteherder" />
           </div>
-          <a 
-            className="new-note" 
-            href="/notes"
-            style={styles.newNote}
-    
-        >
-            <img 
-                src={newHover} 
-                alt="New note"
-                style = {styles.newNoteImg}
-                
-            />
-            <img 
-                className="outline" 
-                src={newIcon} alt="New note" 
-                style = {styles.newNoteImg}     
-            />
+          <a className="new-note" href="/notes">
+            <img src={newHover} alt="New note" />
+            <img className="outline" src={newIcon} alt="New note" />
           </a>
-          <div 
-            className="SignOut"
-            style = {styles.signOut}  
-          >
-            <button
-                style = {styles.btn}
-            >
-              <i 
-                className="fa fa-sign-out"
-                style = {styles.btnIcon}
-              ></i>
+          <div className="SignOut">
+            <button>
+              <i className="fa fa-sign-out"></i>
             </button>
           </div>
         </nav>
     )
-}
-
-const styles = {
-    sidebar: {
-        width: '6rem',
-        backgroundColor: '#f3f3f3',
-        padding: '0.5rem 0',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    logo: {
-        fontFamily: '"Fauna One"',
-        color: '#666',
-        fontSize: '5rem',
-    },
-    logoImg: {
-        width: '3rem',
-        paddingLeft: '0.4rem',
-    },
-    newNote: {
-        marginTop: '2rem',
-        position: 'relative',
-        width: '4rem',
-    },
-    signOut: {
-        position: 'absolute',
-        bottom: '1rem',
-    },
-    btn: {
-        backgroundColor: 'transparent',
-        border: '0',
-        color: '#008BF8',
-        cursor: 'pointer',
-        outline: 'none',
-    },
-    btnIcon: {
-        fontSize: '2rem'
-    },
-    newNoteImg: {
-        position: 'absolute',
-        left: '0',
-        width: '100%',
-        transition: 'opacity 0.25s ease-in-out',
-    }, 
-    imgHover:{
-        opacity: '0',
-    },  
 }
 
 export default Sidebar
