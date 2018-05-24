d9be18e1c2ad9b93135f86019b8ec9a2e8261517
Christine Schulte <christinerschulte@gmail.com>
2018-05-24 14:39:28 -0400

    Add Firebase real-time database

---
+++ src/base.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+
+var config = {
+    apiKey: "AIzaSyBDep8EpawcMpXb3o7UQhDMul6PFW2TiEc",
+    authDomain: "noteherder-f55b0.firebaseapp.com",
+    databaseURL: "https://noteherder-f55b0.firebaseio.com",
+    projectId: "noteherder-f55b0",
+    storageBucket: "noteherder-f55b0.appspot.com",
+    messagingSenderId: "556850868441"
+}
+  firebase.initializeApp(config);
