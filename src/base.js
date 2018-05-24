704cf2f3f16b6103d4520baa3eb12a9890f50716
Christine Schulte <christinerschulte@gmail.com>
2018-05-24 15:20:56 -0400

    Sync notes with Firebase

--- src/base.js
+++ src/base.js
@@ -1,6 +1,8 @@
-import firebase from 'firebase'
+import firebase from 'firebase/app'
+import 'firebase/database'
+import Rebase from 're-base'
 
-var config = {
+const config = {
     apiKey: "AIzaSyBDep8EpawcMpXb3o7UQhDMul6PFW2TiEc",
     authDomain: "noteherder-f55b0.firebaseapp.com",
     databaseURL: "https://noteherder-f55b0.firebaseio.com",
@@ -8,4 +10,6 @@ var config = {
     storageBucket: "noteherder-f55b0.appspot.com",
     messagingSenderId: "556850868441"
 }
-  firebase.initializeApp(config);
+ const app = firebase.initializeApp(config)
+
+ export default Rebase.createClass(app.database())
