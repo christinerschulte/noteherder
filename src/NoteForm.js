d6d11473daddba698e388d4172ca2027e12390df
Christine Schulte <christinerschulte@gmail.com>
2018-05-30 15:01:58 -0400

    Populate the form based on the route param

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -10,6 +10,17 @@ class NoteForm extends Component {
     }
   }
 
+  componentWillReceiveProps = (newProps) =>{
+    const newId = newProps.match.params.id
+    
+    const i = newProps.notes.findIndex(currentNote => currentNote.id.toString() === newId.toString())
+    const note = newProps.notes[i]
+
+    if (note){
+      this.setState({ note })
+    }
+  }
+
   blankNote = () => {
     return {
       id: null,
